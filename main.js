27064cb1ebb57c477adbacd92b1d5e774e804ee1
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2021-01-07 17:08:07 -0300

    feat[html-css]: Add copy instructions with minor styles

--- main.js
+++ main.js
@@ -10,12 +10,36 @@ const userRoleOutput = document.querySelector('.role');
 const userEmailOutput = document.querySelector('.email');
 const userPhoneOutput = document.querySelector('.phone');
 
+// Helper constants
+const isFieldValid = {
+  name: false,
+  role: false,
+  email: false,
+  phone: false,
+};
+
 const updateValue = (e, output) => {
   output.textContent = e.target.value;
+  const isValid = validateInputFields(output.classList[0], e.target.value);
+
+  // if (isValid)
 };
 
 const handleInputAddEventListener = (input, output) => {
   input.addEventListener('input', (e) => updateValue(e, output));
+};
+
+const validateInputFields = (key, content) => {
+  if (content) isFieldValid[key] = true;
+  let iteratorFlag = 0;
+
+  for (const prop in isFieldValid) {
+    if (isFieldValid[prop]) iteratorFlag += 1;
+  }
+
+  if (iteratorFlag === Object.keys(isFieldValid).length) return true;
+
+  return false;
 };
 
 handleInputAddEventListener(userNameInput, userNameOutput)
